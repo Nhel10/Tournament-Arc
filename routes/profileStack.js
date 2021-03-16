237bf006139c8c8c7aaa6a6f7f19422a15bc4b56
Nhel10 <lnyonzon@gmail.com>
2021-03-16 00:46:14 -0700

    manually added John Q profile page

--- routes/profileStack.js
+++ routes/profileStack.js
@@ -1,38 +1,125 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import Profile from '../screens/profileScreen';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { StyleSheet, ScrollView, Text, Image, View } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { block } from "react-native-reanimated";
+import { SocialIcon } from "react-native-elements";
 
+export default function Profile() {
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Card style={style.eventCard}>
+        <View style={style.userCard}>
+          <Image
+            style={style.avatar}
+            source={require("../assets/bienfu.png")}
+          ></Image>
+          <View style={{ flexDirection: "column" }}>
+            <Paragraph style={style.username}>johniscool123</Paragraph>
+            <View style={{flexDirection: "row", marginLeft: 10, marginTop: 5}}>
+              <Image
+                style={style.socials}
+                source={require("../assets/twitch.png")}
+              ></Image>
+              <Image
+                style={style.socials}
+                source={require("../assets/discord.png")}
+              ></Image>
+              <Image
+                style={style.socials}
+                source={require("../assets/twitter.png")}
+              ></Image>
+            </View>
+          </View>
+        </View>
+      </Card>
 
-const { Navigator, Screen } = createStackNavigator();
+      <Card style={style.eventCard}>
+        <Title style={style.headerTitle}>Upcoming Events</Title>
+        <Card>
+          <Image
+            style={style.image}
+            source={require("../assets/intel.jpg")}
+          ></Image>
+          <Title style={style.eventTitle}>
+            Intel Extreme Masters Katowice 2020
+          </Title>
+        </Card>
+        <Card>
+          <Image
+            style={style.image}
+            source={require("../assets/lol2020.png")}
+          ></Image>
+          <Title style={style.eventTitle}>
+            2020 League of Legends World Championship
+          </Title>
+        </Card>
+      </Card>
 
-export const ProfileStack = ({navigation}) => (
-  <Navigator
-    headerMode='screen'
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#304857',
-      },
-      headerTintColor: '#fff',
-      height: 60
-    }}
-  >
-    <Screen
-      name='Profile'
-      component={Profile}
-      options={{ 
-        title: 'Profile Screen', 
-        headerTitleAlign: 'center',
-         headerLeft: () => (
-          <Icon.Button name = "ios-menu" size={25}
-          backgroundColor="#304857" onPress={() => navigation.openDrawer()}
-          ></Icon.Button>
-         )
-        }}
-    />
+      <Card style={style.eventCard}>
+        <Title style={style.headerTitle}>Past Events</Title>
+        <Card>
+          <Image
+            style={style.image}
+            source={require("../assets/dota2019.png")}
+          ></Image>
+          <Title style={style.eventTitle}>
+            The International Dota 2 Championship 2019
+          </Title>
+        </Card>
+        <Card>
+          <Image
+            style={style.image}
+            source={require("../assets/fortnite.jpg")}
+          ></Image>
+          <Title style={style.eventTitle}>Fortnite World Cup 2019</Title>
+        </Card>
+      </Card>
+    </ScrollView>
+  );
+}
 
-
-  </Navigator>
-);
-
-export default ProfileStack;
+const style = StyleSheet.create({
+  userCard: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  socials: {
+    width: 25,
+    height: 25,
+    marginRight: 10,
+  },
+  username: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingTop: 15,
+  },
+  eventCard: {
+    marginTop: 15,
+  },
+  headerTitle: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    fontWeight: "bold"
+  },
+  image: {
+    width: 415,
+    height: 120,
+    resizeMode: "stretch",
+  },
+  eventTitle: {
+    fontSize: 15,
+    paddingLeft: 10,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 60,
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
