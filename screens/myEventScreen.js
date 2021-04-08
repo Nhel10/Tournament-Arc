04acab38b23073e70d069ab3f1e91a322a950713
Nhel10 <lnyonzon@gmail.com>
2021-04-08 02:39:21 -0700

    Updated myEvent Screen

--- screens/myEventScreen.js
+++ screens/myEventScreen.js
@@ -1,11 +1,101 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, ScrollView, Text, Image, View } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
-export default function MyEventScreen() {
+export default function Profile() {
     return (
-        <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>My Event Screen</Text>
-        </View>
-    )
-}
+      <ScrollView style={globalStyles.container}>
+  
+        <Card style={style.eventCard}>
+          <Card>
+            <Image
+              style={style.image}
+              source={require("../assets/night.jpg")}
+            ></Image>
+            <Title style={style.eventTitle}>
+              Planned Under Night Event 1 
+            </Title>
+            <Card.Actions>
+            <Button>View</Button>
+            </Card.Actions>
+          </Card>
+
+          <Card>
+            <Image
+              style={style.image}
+              source={require("../assets/guiltygear.jpg")}
+            ></Image>
+             <Title style={style.eventTitle}>
+              Planned Guilty Gear Event 2 
+            </Title>
+            <Card.Actions>
+            <Button>View</Button>
+            </Card.Actions>
+          </Card>
+
+          <Card>
+            <Image
+              style={style.image}
+              source={require("../assets/blaze.jpg")}
+            ></Image>
+            <Title style={style.eventTitle}>
+              Planned Blazeblue Event 3
+            </Title>
+            <Card.Actions>
+            <Button>View</Button>
+            </Card.Actions>
+          </Card>
+        </Card>
+        
+  
+       
+      </ScrollView>
+    );
+  }
+  
+  const style = StyleSheet.create({
+    userCard: {
+      flex: 1,
+      flexDirection: "row",
+    },
+    socials: {
+      width: 25,
+      height: 25,
+      marginRight: 10,
+    },
+    username: {
+      fontWeight: "bold",
+      fontSize: 20,
+      paddingLeft: 10,
+      paddingTop: 15,
+    },
+    eventCard: {
+      marginTop: 15,
+    },
+    headerTitle: {
+      paddingLeft: 10,
+      paddingTop: 10,
+      paddingBottom: 10,
+      fontSize: 25,
+      fontWeight: "bold"
+    },
+    image: {
+      width: 415,
+      height: 120,
+      resizeMode: "stretch",
+    },
+    eventTitle: {
+      fontSize: 15,
+      paddingLeft: 10,
+    },
+    avatar: {
+      width: 80,
+      height: 80,
+      borderRadius: 60,
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 5,
+    },
+  });
+  
