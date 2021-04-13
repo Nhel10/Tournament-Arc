5205f550bf300ddde7fcfe33d6cf9c3d95ed9714
Richie Ear <richieear@gmail.com>
2021-04-13 10:30:05 -0700

    Tried different approach
    Using App.js since it seems to work better for the imports and trying to get any console info. Source/queries being undefined are major errors popping up

--- App.js
+++ App.js
@@ -1,45 +1,61 @@
 import { Text, View, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as Font from "expo-font";
 import Home from "./screens/home";
 import { AppLoading } from "expo";
 import { AppNavigator } from "./routes/drawer";
-import Amplify, { API } from "aws-amplify";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
 import { Authenticator } from "aws-amplify-react-native";
 import awsconfig from "./src/aws-exports";
-import { crtTour as createTournamentEvent, dltTour as deleteTournamentEvent, updTour as updateTournamentEvent } from './graphql/mutations'
+//import { crtTour as createTournamentEvent, dltTour as deleteTournamentEvent, updTour as updateTournamentEvent } from './graphql/mutations'
+import * as queries from './graphql/queries';
+import { CreateTournamentEvent } from './graphql/queries';
+import * as mutations from './graphql/mutations';
+import * as subscriptions from './graphql/subscriptions';
 
 Amplify.configure(awsconfig);
 
-const initialFormState = { eventName: '', eventDescription: '', eventOrganizingBody: '', eventLocation: '', eventStartDate: '', eventEndDate: '', eventPrizeMoney: '' }
+//const initialFormState = { eventName: '', eventDescription: '', eventOrganizingBody: '', eventLocation: '', eventStartDate: '', eventEndDate: '', eventPrizeMoney: '' }
 
 export default function App() {
-  const [tourInfo, setTourInfo] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+  // const [tourInfo, setTourInfo] = useState([]);
+  // const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(() => {
-    fetchTourInfo();
-  }, []);
+  // useEffect(() => {
+  //   fetchTourInfo();
+  // }, []);
 
-  async function fetchTourInfo() {
-    const apiData = await API.graphql({ query: tourInfo });
-    setNotes(apiData.data.tourInfo.items);
-    console.log('Tournament Event info access granted')
-  }
+  // async function fetchTourInfo() {
+  //   const apiData = await API.graphql({ query: tourInfo });
+  //   setNotes(apiData.data.tourInfo.items);
+  //   console.log('Tournament Event info access granted')
+  // }
 
+  // async function createTournamentEvent() {
+  //   if (!formData.eventName || !formData.eventDescription || !formData.eventOrganizingBody || !formData.eventLocation 
+  //     || !formData.eventStartDate || !formData.eventEndDate || !formData.eventPrizeMoney) return;
+  //   await API.graphql({ query: crtTour, variables: { input: formData } });
+  //   setTourInfo([ ...tourInfo, formData ]);
+  //   setFormData(initialFormState);
+  //   console.log('Tournament Event successfully created')
+  // }
+
+  
   async function createTournamentEvent() {
-    if (!formData.eventName || !formData.eventDescription || !formData.eventOrganizingBody || !formData.eventLocation 
-      || !formData.eventStartDate || !formData.eventEndDate || !formData.eventPrizeMoney) return;
-    await API.graphql({ query: crtTour, variables: { input: formData } });
-    setTourInfo([ ...tourInfo, formData ]);
-    setFormData(initialFormState);
-    console.log('Tournament Event successfully created')
+    try {
+      const createTournament = await API.graphql(graphqlOperation(queries.CreateTournamentEvent)); 
+      console.log(createTournament);
+    } catch (error) {
+      console.log(error);
+    }
+    
   }
 
   return (
     <View style={styles.container}>
       <Authenticator>
         <Text>Home</Text>
+        <button onClick={createTournamentEvent}>Create Tournament</button>
       </Authenticator>
     </View>
   );
--- screens/createTournament.js
+++ screens/createTournament.js
@@ -2,30 +2,8 @@ import React from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-//import { CreateTournamentEvent } from './src/graphql/mutations.graphql';
-import { CreateTournamentEvent } from 'graphql';
-import { crtTour as createTournamentEvent, dltTour as deleteTournamentEvent, updTour as updateTournamentEvent } from './'
-
-//const initialFormState = { eventName: '', eventDescription: '', eventOrganizingBody: '', eventLocation: '', eventStartDate: '', eventEndDate: '', eventPrizeMoney: '' }
 
 export default function CreateTournamentEvent({navigation}) {
-    //https://acloudguru.com/blog/engineering/8-steps-to-building-your-own-serverless-graphql-api-using-aws-amplify used this source
-    //const [eventName, ]
-    state = { eventName: '', eventDescription: '', eventOrganizingBody: '', eventLocation: '', eventStartDate: '', eventEndDate: '', eventPrizeMoney: '' }
-    createEvent = async () => {
-        const tournamentEvent = { 
-            name: this.state.eventName,
-            description: this.state.eventDescription,
-            organizingBody: this.state.eventOrganizingBody,
-            location: this.state.eventLocation,
-            startDate: this.state.eventStartDate,
-            endDate: this.state.eventEndDate,
-            totalPrizeMoney: this.state.eventPrizeMoney }
-        await API.graphql(graphqlOperation(CreateTournamentEvent, tournamentEvent))
-        console.log('Tournament Event successfully created')
-    }
-
-    //async function 
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.pageText}>
