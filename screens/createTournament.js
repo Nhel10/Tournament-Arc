f25d70a4b9effdfb38a534ffffe2fa6d2c1fac87
Richie Ear <richieear@gmail.com>
2021-04-14 18:53:41 -0700

    Praying this reverts

--- screens/createTournament.js
+++ screens/createTournament.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
+<<<<<<< HEAD
 
 export default function CreateTournamentEvent({navigation}) {
+=======
+//import { CreateTournamentEvent } from './src/graphql/mutations.graphql';
+import { CreateTournamentEvent } from 'graphql';
+
+export default function ReviewDetails({navigation}) {
+    //https://acloudguru.com/blog/engineering/8-steps-to-building-your-own-serverless-graphql-api-using-aws-amplify used this source
+    state = { eventName: '', eventDescription: '', eventOrganizingBody: '', eventLocation: '', eventStartDate: '', eventEndDate: '', eventPrizeMoney: '' }
+    createEvent = async () => {
+        const tournamentEvent = { 
+            name: this.state.eventName,
+            description: this.state.eventDescription,
+            organizingBody: this.state.eventOrganizingBody,
+            location: this.state.eventLocation,
+            startDate: this.state.eventStartDate,
+            endDate: this.state.eventEndDate,
+            totalPrizeMoney: this.state.eventPrizeMoney }
+        await API.graphql(graphqlOperation(CreateTournamentEvent, tournamentEvent))
+        console.log('Tournament Event successfully created')
+    }
+>>>>>>> parent of 2cf2f18 (Making some changes)
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.pageText}>
