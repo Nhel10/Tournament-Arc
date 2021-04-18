2a7e69b552d05adcef6971748570036722528333
bjohnnso <johnso428@yahoo.com>
2021-04-17 18:26:05 -0700

    three interface

---
+++ screens/checkInConfirmation.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button, View, Text, Image, TextInput, Switch} from 'react-native';
+import { globalStyles } from '../styles/global';
+
+//Develop an interface for players to check in their competing bracket 
+
+export default function ReviewDetails({navigation}) {
+    const [isEnabled1, setIsEnabled1] = useState(false);
+    const toggleSwitch1 = () => setIsEnabled1(previousState => !previousState);
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.pageText}>
+                 Competitor Check-In
+            
+            </Text>
+
+            <Text style={globalStyles.pageText}>
+                Do you want to check in? 
+                <Switch
+                    trackColor={{ false: "#767577", true: "#e1e4e8" }}
+                    thumbColor={isEnabled1 ? "#f4f3f4" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch1}
+                    value={isEnabled1}
+                />
+            </Text>
+            <Image
+                style={{ width: 400, height: 400}}
+                source={{uri: 'https://images.unsplash.com/photo-1582801396492-705377f39876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80a'}}
+            />
+        </View>
+        
+    )
+
+}
---
+++ screens/testing.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Button, View, Text, Image, TextInput, Switch} from 'react-native';
+import { globalStyles } from '../styles/global';
+
+//Develop an interface for Co-Host or Host to keep track of competitors who checked in
+
+export default function ReviewDetails({navigation}) {
+    const [isEnabled1, setIsEnabled1] = useState(false);
+    const toggleSwitch1 = () => setIsEnabled1(previousState => !previousState);
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 1}}>
+        <Image
+         style={{
+           flex: 1,
+           width:50,
+           height:50,
+         }}
+         source={{uri: 'https://images.smash.gg/images/user/163050/image-b1d42b0b238af2b6302848a5ffdeebef.png'}}
+         />
+         <Text style={{position: 'absolute', fontSize: 40}}>Brad Pitt</Text>
+     </View>
+
+        
+        
+    )
+
+}
