2fbbbce2ae511c8f177bfaf825ff9b5f3c7767f1
John Quach <quachjohnhien@gmail.com>
2021-05-04 02:16:49 -0700

    Update App.js
    Removing Loading Text

--- App.js
+++ App.js
@@ -96,7 +96,6 @@ export class App extends Component {
     if (!loaded) {
       return (
         <View style={{ flex: 1, justifyContent: "center" }}>
-          <Text>Loading</Text>
         </View>
       );
     }
