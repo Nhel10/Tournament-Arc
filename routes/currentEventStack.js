3820a78d52f295e2d32bd82805fcf23093bd35de
Nhel10 <lnyonzon@gmail.com>
2021-05-11 07:57:47 -0700

    ryans stuff part 3
    components/bracket.js
    routes/chatStack.js
    routes/currentEventStack.js

--- routes/currentEventStack.js
+++ routes/currentEventStack.js
@@ -1,52 +1,60 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import CurrentEvent from "../screens/currentEventScreen";
-import CurrentEvents from "../screens/currentEventsScreen";
-import CurrentTournament from "../screens/tourneyArcScreen";
-import CurrentBracket from "../screens/currentBracketScreen";
-import register from "../screens/registerTourneyScreen";
-import Icon from "react-native-vector-icons/Ionicons";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import CurrentEvent from '../screens/currentEventScreen';
+import CurrentEvents from '../screens/currentEventsScreen';
+import CurrentTournament from '../screens/tourneyArcScreen';
+import CurrentBracket from '../screens/displayBracket';
+import register from '../screens/registerTourneyScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 const { Navigator, Screen } = createStackNavigator();
 
-export const CurrentEventStack = ({ navigation }) => (
-  <Navigator
-    headerMode="screen"
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "#304857",
-      },
-      headerTintColor: "#fff",
-      height: 60,
-    }}
-    initialRouteName="Current Events"
-  >
-    <Screen name="Current Events" component={CurrentEvents} />
-
-    <Screen
-      name="Current Event"
-      component={CurrentEvent}
-      options={{
-        headerShown: false,
+export const CurrentEventStack = ({navigation}) => (
+    <Navigator
+      headerMode='screen'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#304857',
+        },
+        headerTintColor: '#fff',
+        height: 60
       }}
-    />
+      initialRouteName = 'Current Events'
+    >
+        <Screen
+            name='Current Events'
+            component={CurrentEvents}
+        />
 
-    <Screen
-      name="Current Tournament"
-      component={CurrentTournament}
-      options={{
-        headerShown: false,
-      }}
-    />
+      <Screen
+        name='Current Event'
+        component={CurrentEvent}
+        options={{ 
+          headerShown: false
+          }}
+      />
 
-    <Screen
-      name="Current Bracket"
-      component={CurrentBracket}
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Screen name="Register" component={register} />
-  </Navigator>
-);
+        <Screen
+            name='Current Tournament'
+            component={CurrentTournament}
+            options={{
+                headerShown: false
+            }}
+        />
 
-export default CurrentEventStack;
+        <Screen
+            name='Current Bracket'
+            component={CurrentBracket}
+            options={{
+                headerShown: false
+            }}
+        />
+      <Screen
+        name='Register'
+        component={register}
+      />
+  
+
+    </Navigator>
+  );
+  
+  export default CurrentEventStack;
