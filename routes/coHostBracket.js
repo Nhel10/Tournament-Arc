f2f8a6361ef1a8f5e263ffb96c1905841e469358
Nhel10 <lnyonzon@gmail.com>
2021-05-11 09:24:03 -0700

    changed some screens

--- routes/coHostBracket.js
+++ routes/coHostBracket.js
@@ -3,12 +3,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import BracketScreen from "../screens/coHostBracketScreen";
 const { Navigator, Screen } = createStackNavigator();
 import Icon from "react-native-vector-icons/Ionicons";
+import bracket from "../screens/displayBracket";
 
 export const CoHostBracket = () => (
   <Navigator>
     <Screen
       name="Bracket Screen"
-      component={BracketScreen}
+      component={bracket}
       options={{
         headerShown: false,
       }}
--- routes/competitorOverviewStack.js
+++ routes/competitorOverviewStack.js
@@ -2,12 +2,13 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import CompCheckin from "../screens/competitorBracket";
 const { Navigator, Screen } = createStackNavigator();
+import bracket from "../screens/displayBracket";
 
 export const CompetitorOverviewStack = () => (
   <Navigator>
     <Screen
       name="OverviewScreen"
-      component={CompCheckin}
+      component={bracket}
       options={{
         headerShown: false,
       }}
